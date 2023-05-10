56e57a3772d989763f84bf52f04d4a73e780f4ea
habibnoori40 <therealhabib0@gmail.com>
2023-05-10 23:39:15 +0500

    first commit

--- functions/getApi.js
+++ functions/getApi.js
@@ -3,9 +3,11 @@ exports.handler = function (event, context, callback) {
     callback(null, {
         statusCode: 200,
         headers: {
-            "Access-Control-Allow-Origin": "https://habibnoori40.github.io",
+            "Access-Control-Allow-Origin": "*",
             "Content-Type": "application/json"
         },
         body: JSON.stringify({ key: apiKey })
     });
 };
+
+// https://habibnoori40.github.io
