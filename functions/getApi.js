585c65b256bc3a91c77cb8e4b76d3099619bf469
habibnoori40 <therealhabib0@gmail.com>
2023-05-10 20:47:05 +0500

    first commit

--- functions/getApi.js
+++ functions/getApi.js
@@ -2,6 +2,10 @@ exports.handler = function (event, context, callback) {
     const apiKey = process.env.API_KEY;
     callback(null, {
         statusCode: 200,
-        body: apiKey
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ key: apiKey })
     });
 };
